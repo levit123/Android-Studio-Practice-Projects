133975ccf44f01d99a0ec3a006a457b2b564a8af
levit123 <LeviCaseBlodgett@gmail.com>
2022-11-02 12:17:46 -0700

    first push

---
+++ app/src/main/java/com/example/firstpracticeproject/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.firstpracticeproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().getDecorView().setBackgroundColor(Color.RED);
+    }
+}
