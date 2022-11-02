73517b71073c64ee1c0a51e949e7915ed424e64c
levit123 <LeviCaseBlodgett@gmail.com>
2022-11-02 12:33:17 -0700

    new module
    -added new Java module to project to get practice with different programming concepts in Java

---
+++ learningJava/src/main/java/com/example/learningjava/MyClass.java
@@ -0,0 +1,23 @@
+package com.example.learningjava;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Hewwo OwO!");
+
+        String name = "Levi";
+        int age = 25;
+
+        System.out.println(name + " is " + age + " years old.");
+
+        boolean iAmCool = false;
+
+        if (iAmCool == true)
+        {
+            System.out.println("This is true");
+        }
+        else
+        {
+            System.out.println("This is false!");
+        }
+    }
+}
