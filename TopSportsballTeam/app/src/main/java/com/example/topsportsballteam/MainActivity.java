1dfd5f68498f51d78dfdb116e45e0e3732699292
levit123 <LeviCaseBlodgett@gmail.com>
2023-01-19 10:35:10 -0800

    fds

--- TopSportsballTeam/app/src/main/java/com/example/topsportsballteam/MainActivity.java
+++ TopSportsballTeam/app/src/main/java/com/example/topsportsballteam/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.topsportsballteam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] players = new String[] {"John Arbuckle", "Sarah Benedito", "Jean Billie", "Billie Sunshine", "Albert Alpacino", "Fettucini Revolution"};
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter<String> playerAdapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_list_item_1, players);
+
+        listView.setAdapter(playerAdapter);
     }
 }
